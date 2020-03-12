73d906047543aa3da8c96763068bc990d2bae6c1
bornast <borna.sunjic.st@gmail.com>
2020-03-12 22:50:02 +0100

    task1-solution3: refactored code using enums

--- Task1/Solution3/Program.cs
+++ Task1/Solution3/Program.cs
@@ -8,27 +8,35 @@ namespace Solution3
     {
         public string number;
         public float amount;
-        public string type;
+        public AccountTypes accountType;
+    }
+
+    public enum AccountTypes
+    {
+        Savings = 0,
+        Current_Account = 1,
+        Giro_Account = 2
     }
 
     class Program
     {
-        static void Main(string[] args)
-        {
-            string[] accountTypes = { "Savings", "Current account", "Giro account" };
+        public const int MaxAccountNumbers = 5;
 
-            var bankAccounts = new BankAccount[5];
+        static void Main(string[] args)
+        {            
+
+            var bankAccounts = new BankAccount[MaxAccountNumbers];
 
             int numberOfAccounts = 0;
 
-            while (true)
+            while (numberOfAccounts <= MaxAccountNumbers)
             {
                 Console.WriteLine("Enter 1 to register a new account, 2 to list all accounts and Exit to leave!");
                 string userInput = Console.ReadLine().ToLower();
 
                 if (userInput.Equals("1"))
                 {
-                    EnterAccount(bankAccounts, numberOfAccounts, accountTypes);
+                    EnterAccount(bankAccounts, numberOfAccounts);
                     numberOfAccounts++;
                 }
                 else if (userInput.Equals("2"))
@@ -46,7 +54,7 @@ namespace Solution3
             }
         }
 
-        private static void EnterAccount(BankAccount[] bankAccounts, int accountIndex, string[] accountTypes)
+        private static void EnterAccount(BankAccount[] bankAccounts, int accountIndex)
         {
             Console.WriteLine("Enter the account number:");
             bankAccounts[accountIndex].number = Console.ReadLine();
@@ -56,9 +64,9 @@ namespace Solution3
             bankAccounts[accountIndex].amount = float.Parse(amountInput, CultureInfo.InvariantCulture);
 
             Console.WriteLine("Enter the account type:");
-            Console.WriteLine("(0 for "+ accountTypes[0] + ", 1 for "+ accountTypes[1] + " and 2 for "+ accountTypes[2] + ")");
+            Console.WriteLine("(0 for Savings, 1 for Current account and 2 for Giro account)");
             ReadIntegerFromConsole(out int accountTypeNumber, new List<int> { 0, 1, 2 });
-            bankAccounts[accountIndex].type = accountTypes[accountTypeNumber];
+            bankAccounts[accountIndex].accountType = (AccountTypes)accountTypeNumber;
         }
 
         private static void PrintAccounts(BankAccount[] bankAccounts)
@@ -66,7 +74,7 @@ namespace Solution3
             foreach (var bankAccount in bankAccounts)
             {
                 if (!string.IsNullOrEmpty(bankAccount.number))
-                    Console.WriteLine("Account number: " + bankAccount.number + " Account amount: " + bankAccount.amount + " Account type: " + bankAccount.type);
+                    Console.WriteLine("Account number: " + bankAccount.number + " Account amount: " + bankAccount.amount + " Account type: " + bankAccount.accountType.ToString().Replace("_", " "));
             }
         }
 
@@ -79,7 +87,7 @@ namespace Solution3
             
             Console.WriteLine("Wrong input value");
             ReadIntegerFromConsole(out number, acceptedValues);
-        }
+        }        
 
     }
 }
