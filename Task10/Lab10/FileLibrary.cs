145e2c6aad426157fd22e3bbc516b4dac6feae11
bornast <borna.sunjic.st@gmail.com>
2020-04-16 14:12:20 +0200

    task 10 finished

---
+++ Task10/Lab10/FileLibrary.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Soap;
+using System.Text;
+
+namespace Labs
+{
+    public static class FileLibrary
+    {
+        private static void CreateFileIfDoesntExists(string path)
+        {
+            if (!File.Exists(path))
+            {
+                FileStream stream = File.Create(path);
+                stream.Close();
+
+            }
+        }
+
+        public static void SerializeObjectToFile(object objectToSerialize, string path)
+        {
+			CreateFileIfDoesntExists(path);
+
+			FileStream stream = new FileStream(path, FileMode.Open);
+
+			SoapFormatter formatter = new SoapFormatter();
+
+			try
+			{
+				formatter.Serialize(stream, objectToSerialize);
+			}
+			catch (SerializationException ex)
+			{
+				Console.WriteLine("Failed to serialize. Reason: " + ex.Message);
+				throw;
+			}
+			finally
+			{
+				stream.Close();
+			}
+		}
+
+		public static object DeserializeObjectFromFile(string path)
+		{
+			var formWindow = new object();
+
+            FileStream fs = new FileStream(path, FileMode.Open);
+
+            try
+            {
+                SoapFormatter formatter = new SoapFormatter();
+
+                formWindow = formatter.Deserialize(fs);
+            }
+            catch (SerializationException e)
+            {
+                Console.WriteLine("Failed to deserialize. Reason: " + e.Message);
+                throw;
+            }
+            finally
+            {
+                fs.Close();
+            }
+
+            return formWindow;
+		}
+
+    }
+}
---
+++ Task10/Lab10/FormWindow.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Labs
+{
+    [Serializable]
+    public class FormWindow
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public int Top { get; set; }
+        public int Left { get; set; }
+        public FormWindowState FormWindowState { get; set; }
+    }
+}
